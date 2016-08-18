8e66d9290b819633126f784b2ebc6928c94184f3
Atikom Juntakool <atikom@squaretrade.com>
2016-08-17 17:54:40 -0700

    Successfully display validation error messages using new form.

--- app/enter-email.component.ts
+++ app/enter-email.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
-// import {User} from "./user";
+import { Control, FormGroup, FormControl, Validators } from '@angular/forms';
 // import { EmailValidator } from  './enter-email.validation';
-
-// import { Router } from '@angular/router';
-// import { FormsModule, FormGroup, FormControl, Validators, Control, ControlGroup, FormBuilder } from '@angular/forms';
-
 
 @Component({
   selector: 'enter-email',
@@ -14,32 +8,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 
 export class EnterEmailComponent implements OnInit {
-
-  // model = new User(1, '', '');
-  // submitted = false;
-  //
-  // form: FormGroup;
-  // constructor(private fb: FormBuilder) {}
-  //
-  // ngOnInit () {
-  //
-  //   this.form = new FormGroup({
-  //       email: new FormControl('', [ Validators.required ])
-  //   });
-  //
-  // }
-  //
-  // onSubmit() {
-  //   this.submitted = true;
-  // }
-  //
-  // active = true;
-  //
-  // submitEmail() {
-  //   this.model = new User(1, '', '');
-  //   this.active = false;
-  //   setTimeout(() => this.active = true, 0);
-  // }
 
   ngOnInit() {}
 
@@ -52,52 +20,29 @@ export class EnterEmailComponent implements OnInit {
     }
   };
 
-  enterEmailForm = new FormGroup({
-    details: new FormGroup({
-      email: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4)
-      ])
-    })
-  });
+  // enterEmailForm = new FormGroup({
+  //   email: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(4)
+  //   ])
+  // });
 
+  enterEmailForm: FormGroup;
+  emailCtrl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(4),
+    Validators.pattern('[A-Za-z]{5}')
+  ]);
+
+  constructor() {
+    this.enterEmailForm = new FormGroup({
+      'email': this.emailCtrl
+    })
+  }
 
   onSubmit() {
     this.submitted = true;
   }
 
-  // active = true;
-  //
-  // submitEmail() {
-  //   this.active = false;
-  //   setTimeout(() => this.active = true, 0);
-  // }
-
-  // person = {
-  //   name: {
-  //     first: '',
-  //     last: ''
-  //   }
-  // };
-  //
-  // myForm = new FormGroup({
-  //   name: new FormGroup({
-  //     first: new FormControl('', [
-  //       Validators.required,
-  //       Validators.minLength(3)
-  //     ]),
-  //     last: new FormControl()
-  //   })
-  // });
-
-  // form = new FormGroup({
-  //   name: new FormGroup({
-  //     first: new FormControl('', Validators.required),
-  //     last: new FormControl('', [
-  //       Validators.required,
-  //       Validators.minLength(3)
-  //     ])
-  //   })
-  // });
-
+  get diagnostic() { return JSON.stringify(this.user); }
 }
