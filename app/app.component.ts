d9878556d172837ab0ac29d4480668cf9eb1e2ab
Atikom Juntakool <atikom@squaretrade.com>
2016-09-12 12:59:42 -0700

    Test from in RC6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl} from '@angular/forms';
+import { Control } from '@angular/common';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+  submitRecepitForm: FormGroup;
+  repairDate: AbstractControl;
+  repairCost: AbstractControl;
+
+  constructor(private builder: FormBuilder) {
+    this.submitRecepitForm = builder.group({
+      'repairDate' : ['', [
+        Validators.required,
+        Validators.minLength(7)
+      ]],
+      'repairCost' : ['', [
+        Validators.required,
+        Validators.minLength(7)
+      ]]
+    });
+    this.repairDate = (<FormGroup>this.submitRecepitForm).controls['repairDate'];
+    this.repairCost = (<FormGroup>this.submitRecepitForm).controls['repairCost'];
+  }
+
+  get value(): string {
+    return JSON.stringify(this.submitRecepitForm.value, null, 2);
+  }
+
+}
